0de8b22cbb8810fe273d0dde70725bf8ef45d657
Tsotetsi Thapelo <info@tsotetsithapelo.co.za>
2017-09-06 20:58:47 +0200

    Add remaining serializers and viewsets.

--- src/apps/api/urls.py
+++ src/apps/api/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 
-from .views import UserViewSet
+from .views import UserViewSet, TripViewSet, ModelViewSet, VehicleViewSet, LocationViewSet, DriverViewSet
 
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, base_name='users')
+router.register(r'models', ModelViewSet, base_name='models')
+router.register(r'vehicles', VehicleViewSet, base_name='vehicles')
+router.register(r'locations', LocationViewSet, base_name='locations')
+router.register(r'drivers', DriverViewSet, base_name='drivers')
+router.register(r'trips', TripViewSet, base_name='trips')
 
 
 urlpatterns = [
--- src/apps/api/views.py
+++ src/apps/api/views.py
@@ -1,19 +1,96 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ModelSerializer, VehicleSerializer, \
+                         LocationSerializer, DriverSerializer, TripSerializer
+from apps.juber.models import Model, Vehicle, Location, Driver, Trip
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     retrieve:
-    Return the given user.
+    Returns the given user.
 
     list:
-    Return a list of all the existing users.
+    Returns a list of all the existing users.
 
     create:
     Create a new user instance.
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class ModelViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+    Returns the given model.
+
+    list:
+    Returns list of all models.
+
+    create:
+    Create new model instance.
+    """
+    queryset = Model.objects.all()
+    serializer_class = ModelSerializer
+
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+    Return the given vehicle.
+
+    list:
+    Returns list of all vehicles.
+
+    create:
+    Creates a new vehicle instance.
+    """
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+
+
+class LocationViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+    Return the given location.
+
+    list:
+    Returns list of all locations.
+
+    create:
+    Creates a new location instance.
+    """
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+
+class DriverViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+    Return the given driver.
+
+    list:
+    Returns list of all drivers.
+
+    create:
+    Creates a new driver instance.
+    """
+    queryset = Driver.objects.all()
+    serializer_class = DriverSerializer
+
+
+class TripViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+    Return the given trip.
+
+    list:
+    Returns list of all trips.
+
+    create:
+    Creates a new trip instance.
+    """
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
